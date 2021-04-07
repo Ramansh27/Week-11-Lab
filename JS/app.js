7e3ff527703d2d630430570f4b24b56b9b7a35cc
Ramansh Chhabra <rchhabra6@my.bcit.ca>
2021-04-07 03:17:59 -0700

    empty array created, for loop created to check for status of task

--- JS/app.js
+++ JS/app.js
@@ -3,5 +3,12 @@ const todoList = [
     {todo: "link css and JS", status: "completed"},
     {todo: "write array", status: "started"},
     {todo: "push complete", status: "started"}
-
 ]
+
+const completedTodos = []
+
+for (const task of todoList){
+    if (task.status === "completed"){
+        completedTodos.push(task)
+    }
+}
